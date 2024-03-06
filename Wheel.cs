c1e4c6ef1000796483c15fe6ec3c6849990e5b04
Sai Sunil Gangupamu <gs153@usf.edu>
2024-03-06 16:05:17 -0500

    Added tyre class

---
+++ Wheel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Motorcycle_Project
+{
+    internal class Wheel
+    {
+        public static string diameter;
+        public static string tyretype;
+
+        // default constructor
+        public Wheel()
+        {
+            diameter = "15 inches ";
+            tyretype = " tubeless ";
+        }
+
+
+        public void rotate()
+        {
+            Console.WriteLine(" rotating wheels ");
+        }
+
+
+    }
+}
