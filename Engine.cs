bb0400de168f35b9ccd8a78777191989e0a1aadf
Naga Gayathiri Devi Kasturi <nagagayathiridevi@usf.edu>
2024-03-06 16:05:30 -0500

    psuhing changes

---
+++ Engine.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Motorcycle_Project
+{
+    internal class Engine
+    {
+        public static string type;
+        public static string fueltype;
+        public static string displacement;
+
+        public void Ignite()
+        {
+            Console.WriteLine("Iginitation starts");
+        }
+
+        // Default Constructor
+        public Engine()
+        {
+            type = "V - twin";
+            fueltype = "Gasoline";
+            displacement = "500CC";
+
+        }
+
+      
+    }
+}
