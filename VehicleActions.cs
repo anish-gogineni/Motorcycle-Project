a53ad41f15db832b25b1349d5a082eddfec5bf2c
Anish Babu Gogineni <agogineni@usf.edu>
2024-03-06 16:00:22 -0500

    Created a VechicleActions interface

---
+++ VehicleActions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Motorcycle_Project
+{
+    // Creating an interface called VehicleActions
+    internal interface VehicleActions
+    {
+        // Start method
+        string Start();
+
+        // Stop method
+        string Stop();
+
+        // Accelerate method
+        string Accelerate();
+
+        // Brake method
+        string Brake();
+
+    }
+}
